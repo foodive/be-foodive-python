fab2afcbba8351134109acec8e6b7d40a610d0fe
Jim Riddle <jimriddle0@gmail.com>
2022-08-27 15:20:51 -0500

    create migrations

--- be-foodive-python/foodive/settings.py
+++ be-foodive-python/foodive/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'foodive',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,10 +76,14 @@ WSGI_APPLICATION = 'foodive.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+		'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': 'foodive_development',
+		'USER': 'jamesriddle',
+		'PASSWORD': '1234',
+		'HOST': '127.0.0.1',
+		'PORT': '5432',
+        }
 }
 
 
