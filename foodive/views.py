e17e78c88f82dbe1ae277f4b1fbfc591b0246252
Luke Swenson <LukeSwenson95@outlook.com>
2022-08-29 11:43:16 -0600

    added serializer and edited models

--- foodive/views.py
+++ foodive/views.py
@@ -13,15 +13,22 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
-def restaurant_data(request):
+
+def restaurant_data():
     category = 'mexican'
     location = 'chicago'
+
     # response = requests.get('[https://api.yelp.com/v3/businesses/search?categories=&{category}&location={location}],headers={'Authorization: Bearer <API KEY>'})')
     # requests.get("https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972",headers={'Authorization: Bearer <API KEY>'})
-    requests.get("https://api.yelp.com/v3/businesses/search?categories=&{category}&location={location}",headers={'Authorization: Bearer <API KEY>'})
+    response = requests.get('https://api.yelp.com/v3/businesses/search?location=chicago', headers={'Authorization': 'Bearer llFKl4YQSGryN4AvEdCkWS13yYb-sNbueMcFrEvwGBdBu8Sk8RIG3kSeod9p5ksV2CJfk969a8AoCA4EovVLl3FgCDZ6ZQeHZ0jGslV4VyHHxkKKiYt7ENagrCjxYnYx'})
+
 
     restaurant = response.json()
-    print(restaurant)
-    return HttpResponse("restaurant")
+
+    return restaurant
     pass
+
+
+
