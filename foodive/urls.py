9fbbdd3ce886c100392329f29e2a36976571c423
Jim Riddle <jimriddle0@gmail.com>
2022-08-29 17:07:24 -0500

    add restaurant path

--- foodive/urls.py
+++ foodive/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from foodive import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('restaurants/', views.restaurant_data),
+
 ]
