2d65b6024a9ad45c68f50693c32d1a2c25ae435c
Jim Riddle <jimriddle0@gmail.com>
2022-08-29 17:07:42 -0500

    modify view to print all json about chicago restaurants

--- foodive/views.py
+++ foodive/views.py
@@ -1,34 +1,16 @@
-# from django.http import JsonResponse
-# from .models import User
-# from .serializers import UserSerializer
-#
-# def user_list(request):
-#
-#     #get all drinks, serailize them, return json
-#
-#     drinks = Drink.objects.all()
-#
-#     serailizer = DrinkSerializer(drinks, many=True)
-#     return JsonResponse({"drinks": serailizer.data}, safe=False)
-
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
 import requests
+# from .serializers import RestaurantSerializer
 
 
-def restaurant_data():
-    category = 'mexican'
-    location = 'chicago'
-
-    # response = requests.get('[https://api.yelp.com/v3/businesses/search?categories=&{category}&location={location}],headers={'Authorization: Bearer <API KEY>'})')
-    # requests.get("https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972",headers={'Authorization: Bearer <API KEY>'})
+def restaurant_data(request):
     response = requests.get('https://api.yelp.com/v3/businesses/search?location=chicago', headers={'Authorization': 'Bearer llFKl4YQSGryN4AvEdCkWS13yYb-sNbueMcFrEvwGBdBu8Sk8RIG3kSeod9p5ksV2CJfk969a8AoCA4EovVLl3FgCDZ6ZQeHZ0jGslV4VyHHxkKKiYt7ENagrCjxYnYx'})
 
 
     restaurant = response.json()
-
-    return restaurant
+    # serializer = RestaurantSerializer(response, many=True)
+    # return JsonResponse({"data": response.json()}, safe=False)
+    return JsonResponse({"restaurants": restaurant}, safe=False)
     pass
-
-
-
