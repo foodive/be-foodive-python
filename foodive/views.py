4ed710c4454456aab4da5d85d0df1c1058d9b871
Luke Swenson <LukeSwenson95@outlook.com>
2022-08-30 09:32:05 -0600

    Added api config file to hide api_key

--- foodive/views.py
+++ foodive/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 from django.http import HttpResponse
 from django.http import JsonResponse
 from .config import yelp_api_key
@@ -10,7 +11,7 @@ def restaurant_data(request):
     # response = requests.get('https://api.yelp.com/v3/businesses/search?location=chicago', headers={'Authorization': 'Bearer llFKl4YQSGryN4AvEdCkWS13yYb-sNbueMcFrEvwGBdBu8Sk8RIG3kSeod9p5ksV2CJfk969a8AoCA4EovVLl3FgCDZ6ZQeHZ0jGslV4VyHHxkKKiYt7ENagrCjxYnYx'})
     response = requests.get('https://api.yelp.com/v3/businesses/search?location=chicago', headers={'Authorization': yelp_api_key})
 
-
+    # HttpRequest.content_params
     restaurant = response.json()
     # serializer = RestaurantSerializer(response, many=True)
     # return JsonResponse({"data": response.json()}, safe=False)
