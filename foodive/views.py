35b457dfdbe59fb1545737f9598dc786c8576b26
Jim Riddle <jimriddle0@gmail.com>
2022-08-30 11:08:51 -0500

    modified view to handle querey params

--- foodive/views.py
+++ foodive/views.py
@@ -9,7 +9,10 @@ import requests
 
 def restaurant_data(request):
     # response = requests.get('https://api.yelp.com/v3/businesses/search?location=chicago', headers={'Authorization': 'Bearer llFKl4YQSGryN4AvEdCkWS13yYb-sNbueMcFrEvwGBdBu8Sk8RIG3kSeod9p5ksV2CJfk969a8AoCA4EovVLl3FgCDZ6ZQeHZ0jGslV4VyHHxkKKiYt7ENagrCjxYnYx'})
-    response = requests.get('https://api.yelp.com/v3/businesses/search?location=chicago', headers={'Authorization': yelp_api_key})
+    # location = request.GET.get('location','')
+    payload = {'location': request.GET.get('location','')}
+    # import ipdb; ipdb.set_trace()
+    response = requests.get('https://api.yelp.com/v3/businesses/search', params=payload, headers={'Authorization': yelp_api_key})
 
     # HttpRequest.content_params
     restaurant = response.json()
