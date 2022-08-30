8444e2f482557c6bd61f47cd2269715661906984
Jim Riddle <jimriddle0@gmail.com>
2022-08-30 15:27:22 -0500

    handrolled the json response

--- foodive/views.py
+++ foodive/views.py
@@ -4,19 +4,142 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from .config import yelp_api_key
 import requests
+import json
+import random
 # from .serializers import RestaurantSerializer
 
 
 def restaurant_data(request):
-    # response = requests.get('https://api.yelp.com/v3/businesses/search?location=chicago', headers={'Authorization': 'Bearer llFKl4YQSGryN4AvEdCkWS13yYb-sNbueMcFrEvwGBdBu8Sk8RIG3kSeod9p5ksV2CJfk969a8AoCA4EovVLl3FgCDZ6ZQeHZ0jGslV4VyHHxkKKiYt7ENagrCjxYnYx'})
-    # location = request.GET.get('location','')
     payload = {'location': request.GET.get('location','')}
-    # import ipdb; ipdb.set_trace()
     response = requests.get('https://api.yelp.com/v3/businesses/search', params=payload, headers={'Authorization': yelp_api_key})
 
-    # HttpRequest.content_params
     restaurant = response.json()
-    # serializer = RestaurantSerializer(response, many=True)
-    # return JsonResponse({"data": response.json()}, safe=False)
-    return JsonResponse({"restaurants": restaurant}, safe=False)
+    rand_num = random.randint(0,19)
+    rand_rest = restaurant['businesses'][rand_num]
+    sample_dict = {
+      "type": "object",
+      "required": [
+        "name",
+        "image_url",
+        "categories",
+        "rating",
+        "coordinates",
+        "price",
+        "location",
+        "display_phone"
+      ],
+      "properties": {
+        "name": {
+          "type": "string",
+          "examples": [
+            rand_rest['name']
+          ]
+          },
+        "image_url": {
+          "type": "string",
+          "examples": [
+            rand_rest['image_url']
+          ]
+        },
+        "categories": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "required": [
+              "alias",
+              "title"
+            ],
+            "properties": {
+              "alias": {
+                "type": "string",
+                "examples": [
+                  rand_rest['categories'][0]['alias']
+                ]
+              },
+              "title": {
+                "type": "string",
+                "examples": [
+                  rand_rest['categories'][0]['title']
+                ]
+              }
+            },
+            "examples": [{
+              "alias": "newamerican",
+              "title": "American (New)"
+            },
+             { "alias": "bakeries",
+               "title": "Bakeries"
+             },
+              {
+                "alias": "coffee",
+                "title": "Coffee & Tea"
+            }]
+        }
+      },
+        "rating": {
+          "type": "float",
+          "default": 0.0,
+          "examples": [
+            rand_rest['rating']
+          ]
+        },
+        "coordinates": {
+          "type": "float",
+          "required": [
+            "latitude",
+            "longitude"
+          ],
+          "properties": {
+            "latitude": {
+              "type": "float",
+              "examples": [
+                rand_rest['coordinates']['latitude']
+              ]
+            },
+            "longitude": {
+              "type": "float",
+              "examples": [
+                rand_rest['coordinates']['longitude']
+              ]
+            }
+          },
+          "examples": [{
+            "latitude": rand_rest['coordinates']['latitude'],
+            "longitude": rand_rest['coordinates']['longitude']
+          }]
+        },
+        "price": {
+          "type": "string",
+          "examples": [
+            rand_rest['price']
+          ]
+        },
+        "location": {
+          "type": "object",
+          "required": [
+            "displayed_address"
+          ],
+          "properties": {
+            "display_address":{
+              "type": "array",
+              "items": {
+                "type": "string",
+                "examples": [
+                  rand_rest['location']['display_address']
+                ]
+              }
+            }
+          }
+        },
+        "display_phone": {
+          "type": "string",
+          "examples": [
+            rand_rest['display_phone']
+            ]
+          }
+      }
+    }
+
+    # import ipdb; ipdb.set_trace()
+    return JsonResponse({"restaurants": sample_dict}, safe=False)
     pass
