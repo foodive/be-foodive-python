cf7a90743de5a352437b0a674c90c482ea99d7b7
Luke Swenson <LukeSwenson95@outlook.com>
2022-08-31 17:28:26 -0600

    added comma

--- foodive/settings.py
+++ foodive/settings.py
@@ -122,7 +122,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'https://foodive-be.herokuapp.com',
     'https://foodive.herokuapp.com',
-    'http://localhost:3000'
+    'http://localhost:3000',
 ]
 
 # CORS_ALLOW_METHODS = [
