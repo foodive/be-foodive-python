7af48c42907173fc3c4b6cc37d3396f0f70be471
Jim Riddle <jimriddle0@gmail.com>
2022-09-02 11:29:14 -0500

    write 4 tests for the function within the view

---
+++ foodive/tests.py
@@ -0,0 +1,53 @@
+# content of test_sample.py
+from django.test import TestCase
+# from django.test import unittest
+from django.test import Client
+from foodive.models import User
+from foodive.views import restaurant_data
+
+from django.shortcuts import render
+from django.http import HttpRequest
+from django.http import HttpResponse
+from django.http import JsonResponse
+import requests
+import json
+import random
+from .settings import YELP_API_KEY
+
+def test_view_url_exists_at_desired_location():
+    response = requests.get('https://api.yelp.com/v3/businesses/search?categories=&mexican&chinese&italian&location=chicago', headers={'Authorization': f'Bearer {YELP_API_KEY}'})
+    assert response.status_code == 200
+
+def test_view_url_returns_invalid_status_code_given_bad_input():
+    response = requests.get('https://api.yelp.com/v3/businesses/search?', headers={'Authorization': f'Bearer {YELP_API_KEY}'})
+    assert response.status_code == 400
+
+def test_view_url_has_price_in_dict():
+    response = requests.get('https://api.yelp.com/v3/businesses/search?categories=&mexican&chinese&italian&location=chicago', headers={'Authorization': f'Bearer {YELP_API_KEY}'})
+
+    restaurants = response.json()
+    businesses = restaurants['businesses']
+    rand_num = random.randint(0,len(businesses) - 1)
+    rand_rest = businesses[rand_num]
+    rand_rest_categories = rand_rest['categories']
+
+    if not 'price' in rand_rest:
+        rand_rest['price'] = "n/a"
+
+    assert 'price' in rand_rest
+
+def test_view_url_has_categories_as_list():
+    response = requests.get('https://api.yelp.com/v3/businesses/search?categories=&mexican&chinese&italian&location=chicago', headers={'Authorization': f'Bearer {YELP_API_KEY}'})
+
+    restaurants = response.json()
+    businesses = restaurants['businesses']
+    rand_num = random.randint(0,len(businesses) - 1)
+    rand_rest = businesses[rand_num]
+    rand_rest_categories = rand_rest['categories']
+
+    category_list = []
+
+    for i in rand_rest['categories']:
+        category_list.append(i['title'])
+
+    assert isinstance(category_list, list)
