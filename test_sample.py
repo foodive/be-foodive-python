0183a01d16c54f72a00156e00cb0f72b8ca5eff3
Jim Riddle <jimriddle0@gmail.com>
2022-09-02 11:29:24 -0500

    tinkered with existing tests

--- test_sample.py
+++ test_sample.py
@@ -1,7 +1,7 @@
-# content of test_sample.py
-def func(x):
-    return x + 1
-
-
-def test_answer():
-    assert func(4) == 5
+# # content of test_sample.py
+# def func(x):
+#     return x + 1
+#
+#
+# def test_answer():
+#     assert func(4) == 5
