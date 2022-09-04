5cfbd0b99df942b21ef512c54b0a0d17b271f0c3
Luke Swenson <LukeSwenson95@outlook.com>
2022-09-04 13:24:55 -0600

    Parsed out serializer, created helper method, created class

--- foodive/views.py
+++ foodive/views.py
@@ -2,49 +2,27 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.http import HttpResponse
 from django.http import JsonResponse
-# from .config import yelp_api_key
+
 import requests
 import json
 import random
+
+from .serializers import RestaurantSerializer
 from .settings import YELP_API_KEY
 
-def restaurant_data(request):
-    payload = {'location': request.GET.get('location',''),'categories': request.GET.get('categories','')}
+class Restaurant:
+  def restaurant_data(request):
+      payload = {'location': request.GET.get('location',''),'categories': request.GET.get('categories','')}
 
-    response = requests.get('https://api.yelp.com/v3/businesses/search', params=payload, headers={'Authorization': f'Bearer {YELP_API_KEY}'})
+      response = requests.get('https://api.yelp.com/v3/businesses/search', params=payload, headers={'Authorization': f'Bearer {YELP_API_KEY}'})
 
-    restaurants = response.json()
-    businesses = restaurants['businesses']
-    rand_num = random.randint(0,len(businesses) - 1)
-    rand_rest = businesses[rand_num]
-    rand_rest_categories = rand_rest['categories']
+      restaurants = response.json()
+      businesses = restaurants['businesses']
+      rand_num = random.randint(0,len(businesses) - 1)
+      return businesses[rand_num]
+      
+  def restaurant_response(request):
+      serializer = RestaurantSerializer.serialize_data(Restaurant.restaurant_data(request))
 
-    category_list = []
-
-    for i in rand_rest['categories']:
-        category_list.append(i['title'])
-
-    if not 'price' in rand_rest:
-        rand_rest['price'] = "n/a"
-
-
-    sample_dict = {
-        "id": "null",
-        "type": "restaurant_info",
-        "attributes": {
-          "name": rand_rest['name'],
-          "image_url": rand_rest['image_url'],
-          "categories": category_list,
-          "rating": rand_rest['rating'],
-          "coordinates": {
-            "latitude": rand_rest['coordinates']['latitude'],
-            "longitude": rand_rest['coordinates']['longitude']
-          },
-          "price": rand_rest['price'],
-          "display_address": rand_rest['location']['display_address'],
-          "display_phone": rand_rest['display_phone'],
-    }
-  }
-
-    return JsonResponse({"data": sample_dict}, safe=False)
-    pass
+      return JsonResponse({"data": serializer}, safe=False)
+      pass
