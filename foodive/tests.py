0a47e2aa187ba4fb8874285cb2c9221cd2a06e63
Becky Nisttahuz <benistta@hotmail.com>
2022-09-04 14:55:29 -0600

    Feat:add serialized test
    Co-authored-by: Luke lukeswenson95@outlook.com

--- foodive/tests.py
+++ foodive/tests.py
@@ -1,9 +1,9 @@
-# content of test_sample.py
 from django.test import TestCase
-# from django.test import unittest
 from django.test import Client
 from foodive.models import User
-from foodive.views import restaurant_data
+import foodive.views
+import foodive.serializers
+# import requests
 
 from django.shortcuts import render
 from django.http import HttpRequest
@@ -13,6 +13,7 @@ import requests
 import json
 import random
 from .settings import YELP_API_KEY
+from django.test.client import RequestFactory
 
 def test_view_url_exists_at_desired_location():
     response = requests.get('https://api.yelp.com/v3/businesses/search?categories=&mexican&chinese&italian&location=chicago', headers={'Authorization': f'Bearer {YELP_API_KEY}'})
@@ -51,3 +52,42 @@ def test_view_url_has_categories_as_list():
         category_list.append(i['title'])
 
     assert isinstance(category_list, list)
+
+def test_serializer():
+    rf = RequestFactory()
+    get_request = rf.get('/restaurants/?location=denver&categories=mexican')
+
+    rand_rest = foodive.views.Restaurant.restaurant_data(get_request)
+
+    category_list = []
+
+    for i in rand_rest['categories']:
+        category_list.append(i['title'])
+
+        serialize_data = {
+            "id": "null",
+            "type": "restaurant_info",
+            "attributes": {
+            "name": rand_rest['name'],
+            "image_url": rand_rest['image_url'],
+            "categories": category_list,
+            "rating": rand_rest['rating'],
+            "coordinates": {
+                "latitude": rand_rest['coordinates']['latitude'],
+                "longitude": rand_rest['coordinates']['longitude']
+            },
+            "price": rand_rest['price'],
+            "display_address": rand_rest['location']['display_address'],
+            "display_phone": rand_rest['display_phone'],
+            }
+        }
+    assert isinstance(serialize_data['attributes']['name'], str)
+    assert isinstance(serialize_data['attributes']['image_url'], str)
+    assert isinstance(serialize_data['attributes']['categories'], list)
+    assert isinstance(serialize_data['attributes']['rating'], float)
+    assert isinstance(serialize_data['attributes']['coordinates'], dict)
+    assert isinstance(serialize_data['attributes']['coordinates']['latitude'], float)
+    assert isinstance(serialize_data['attributes']['coordinates']['longitude'], float)
+    assert isinstance(serialize_data['attributes']['price'], str)
+    assert isinstance(serialize_data['attributes']['display_address'], list)
+    assert isinstance(serialize_data['attributes']['display_phone'], str)
