80c78e92694f940830ea3bb5cc5f976b5b1b1fc0
Luke Swenson <LukeSwenson95@outlook.com>
2022-09-04 13:24:15 -0600

    added serializer class and method

--- foodive/serializers.py
+++ foodive/serializers.py
@@ -1,9 +1,32 @@
 from rest_framework import serializers
-import rest_framework.views
+import foodive.views
 
-class RestaurantSerializer(serializers.ModelSerializer):
-    class Meta:
-        # import ipdb; ipdb.set_trace()
-        x = rest_framework.views.restaurant_data()
-        fields = x['businesses'][0]['name']
-        # import ipdb; ipdb.set_trace()
+class RestaurantSerializer:
+    def serialize_data(rand_rest):
+        rand_rest_categories = rand_rest['categories']
+
+        category_list = []
+
+        for i in rand_rest['categories']:
+          category_list.append(i['title'])
+
+        if not 'price' in rand_rest:
+          rand_rest['price'] = "n/a"
+        
+        return {
+            "id": "null",
+            "type": "restaurant_info",
+            "attributes": {
+            "name": rand_rest['name'],
+            "image_url": rand_rest['image_url'],
+            "categories": category_list,
+            "rating": rand_rest['rating'],
+            "coordinates": {
+                "latitude": rand_rest['coordinates']['latitude'],
+                "longitude": rand_rest['coordinates']['longitude']
+            },
+            "price": rand_rest['price'],
+            "display_address": rand_rest['location']['display_address'],
+            "display_phone": rand_rest['display_phone'],
+        }
+    }
