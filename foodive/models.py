c925a11b51897c2e7cace12eb8e6908850c3ece3
Becky Nisttahuz <benistta@hotmail.com>
2022-09-05 10:04:56 -0600

    Feat:add return name and location for user for testing purposes

--- foodive/models.py
+++ foodive/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.name}{self.location}'
